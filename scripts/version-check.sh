ed02cb3deae4c5b524df770540c00d549ffd8385
Julian Liu <wlootlxt123@gmail.com>
2020-01-01 10:21:35 +0800

    Tighten the days limit

--- scripts/version-check.sh
+++ scripts/version-check.sh
@@ -8,7 +8,7 @@ SECONDS_BETWEEN=$(($(date -u +%s) - ${COMMIT_TIMESTAMP}))
 
 DAYS_SINCE=$((SECONDS_BETWEEN / 86400))
 
-if [[ ${DAYS_SINCE} -ge 5 ]]; then
+if [[ ${DAYS_SINCE} -ge 2 ]]; then
     if [[ ${FLAG_OUTDATED} -eq 2 ]]; then
 	    exit 1
 	fi
